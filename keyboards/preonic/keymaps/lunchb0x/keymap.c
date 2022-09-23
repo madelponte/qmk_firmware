87c0b4b84b54b5d2b5b8acb5edc3fd16e336ca40
Marcus DelPonte <marcus.delponte@noaa.gov>
2022-09-23 11:18:02 -0400

    add page up and down

--- keyboards/preonic/keymaps/lunchb0x/keymap.c
+++ keyboards/preonic/keymaps/lunchb0x/keymap.c
@@ -104,8 +104,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC,
     _______, _______, _______, _______, _______, _______, KC_NLCK, KC_P1,    KC_P2,   KC_P3,   KC_PSLS, KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, KC_P4,    KC_P5,   KC_P6,   KC_PAST, KC_PENT,
-    _______, _______, _______, TCOPY,   TPSTE,   _______, _______, KC_P7,    KC_P8,   KC_P9,   KC_PMNS, KC_PSCR,
-    _______, _______, _______, _______, _______, _______, _______, _______,  KC_P0,   KC_PDOT, KC_PPLS, _______
+    KC_PGUP, _______, _______, TCOPY,   TPSTE,   _______, _______, KC_P7,    KC_P8,   KC_P9,   KC_PMNS, KC_PSCR,
+    KC_PGDN, _______, _______, _______, _______, _______, _______, _______,  KC_P0,   KC_PDOT, KC_PPLS, _______
 ),
 
 /* Adjust (Lower + Raise)
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, RESET,   DEBUG,   DM_REC1, DM_PLY1, DM_RSTP, DM_REC2, DM_PLY2, TERM_ON, TERM_OFF, _______, KC_DEL,
+  _______, RESET,   DEBUG,   DM_REC1, DM_PLY1, DM_RSTP, DM_REC2, DM_PLY2, _______, _______, _______, KC_DEL,
   _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, KC_HOME, QWERTY,  _______, _______,  _______, _______,
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
