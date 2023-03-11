cc0c0efa10fed3c9c8184885cf3af05070fd1ab5
Marcus DelPonte <marcus.delponte@noaa.gov>
2023-03-11 09:03:15 -0500

    update preonic/lunchb0x

--- keyboards/preonic/keymaps/lunchb0x/keymap.c
+++ keyboards/preonic/keymaps/lunchb0x/keymap.c
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_NUMPAD] = LAYOUT_preonic_grid(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC,
-    _______, _______, _______, _______, _______, _______, KC_NLCK, KC_P1,    KC_P2,   KC_P3,   KC_PSLS, KC_DEL,
+    _______, _______, _______, _______, _______, _______, KC_LNUM, KC_P1,    KC_P2,   KC_P3,   KC_PSLS, KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, KC_P4,    KC_P5,   KC_P6,   KC_PAST, KC_PENT,
     KC_PGUP, _______, _______, TCOPY,   TPSTE,   _______, _______, KC_P7,    KC_P8,   KC_P9,   KC_PMNS, KC_PSCR,
     KC_PGDN, _______, _______, _______, _______, _______, _______, _______,  KC_P0,   KC_PDOT, KC_PPLS, _______
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |RGBprv| Reset|DEBUG | REC1 |PLAY1 |STPREC| REC2 |PLAY2 |      |      |      |  Del |
+ * |RGBprv| Reset|      | REC1 |PLAY1 |STPREC| REC2 |PLAY2 |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |RGBnxt|      |      |Aud on|AudOff|      | Home |Qwerty|Colmak|      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -147,9 +147,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, RESET,   DEBUG,   DM_REC1, DM_PLY1, DM_RSTP, DM_REC2, DM_PLY2, _______, _______, _______, KC_DEL,
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, KC_HOME, QWERTY, COLEMAK, _______,  _______, _______,
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+  _______, RESET,   _______,   DM_REC1, DM_PLY1, DM_RSTP, DM_REC2, DM_PLY2, _______, _______, _______, KC_DEL,
+  _______, _______, MI_MOD,  AU_ON,   AU_OFF,  _______, KC_HOME, QWERTY, COLEMAK, _______,  _______, _______,
+  _______, AU_PREV,  AU_NEXT,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
