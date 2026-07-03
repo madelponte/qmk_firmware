b2b71088750b068a32170408e0774d2a8cecae18
Marcus DelPonte <madelponte@pm.me>
2026-07-02 20:37:49 -0400

    update preonic keymap

--- keyboards/preonic/keymaps/lunchb0x/keymap.c
+++ keyboards/preonic/keymaps/lunchb0x/keymap.c
@@ -16,13 +16,11 @@ enum preonic_layers {
 enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
-  DYNAMIC_MACRO_RANGE,
   LOWER,
-  RAISE,
-  NUMPAD
+  RAISE
 };
 
-#define NUMPAD MO(_NUMPAD)
+#define NMPD MO(_NUMPAD)
 #define TCOPY LCTL(KC_INS)
 #define TPSTE LCTL(LSFT(KC_V))
 
@@ -46,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_QUOT,
-  KC_LCTL, NUMPAD, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
+  KC_LCTL, NMPD,   KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Colemak
@@ -67,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
   KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_UP, KC_QUOT,
-  KC_LCTL, NUMPAD, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN,   KC_RGHT
+  KC_LCTL, NMPD,   KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
 
@@ -77,9 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | Vol- | Vol+ | Prev |      |             |      | Next |      |      | Play |
  * `-----------------------------------------------------------------------------------'
@@ -98,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      | Pg up|      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      | Pg up|  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      | prev |      |             |      | Next |      | Pg dn| Play |
  * `-----------------------------------------------------------------------------------'
@@ -115,19 +113,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Numpad
  * ,-----------------------------------------------------------------------------------.
- * |  F3  |      |      |      |      |      |NumLck|   1  |   2  |   3  |   /  | Bksp |
+ * |      |      |      |      |      |      |NumLck|   1  |   2  |   3  |   /  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |   4  |   5  |   6  |   *  |ENTER |
+ * |      |      |      |      |      |      |      |   4  |   5  |   6  |   *  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |TCOPY |TPASTE|      |      |   7  |   8  |   9  |   -  |PRINT |
+ * |      |      |      |      |      |      |      |   7  |   8  |   9  |   -  |ENTER |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |   0  |   .  |   +  |      |
+ * | PgUp |      |      |TCOPY |TPSTE |      |      |      |   0  |   .  |   +  |Print |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |RGBTOG|      |      |      |      |             |      |      |      |      |      |
+ * | PgDn |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMPAD] = LAYOUT_preonic_grid(
-    _______, _______, _______, _______, _______, _______, KC_LNUM, KC_P1,    KC_P2,   KC_P3,   KC_PSLS, KC_BSPC,
+    _______, _______, _______, _______, _______, _______, KC_NUM,  KC_P1,    KC_P2,   KC_P3,   KC_PSLS, KC_BSPC,
     _______, _______, _______, _______, _______, _______, _______, KC_P4,    KC_P5,   KC_P6,   KC_PAST, KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, KC_P7,    KC_P8,   KC_P9,   KC_PMNS, KC_PENT,
     KC_PGUP, _______, _______, TCOPY,   TPSTE,   _______, _______, _______,  KC_P0,   KC_PDOT, KC_PPLS, KC_PSCR,
@@ -138,18 +136,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |RGBprv| Reset|      | REC1 |PLAY1 |STPREC| REC2 |PLAY2 |      |      |      |  Del |
+ * |      | Boot |      | REC1 |PLAY1 |STPREC| REC2 |PLAY2 |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |RGBnxt|      |      |Aud on|AudOff|      | Home |Qwerty|Colmak|      |      |      |
+ * |      |      |MIDI M|Aud on|AudOff|      | Home |Qwerty|Colmak|      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |RGBTOG|      |      |      |      |             |      |      |      |      |      |
+ * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, RESET,   _______,   DM_REC1, DM_PLY1, DM_RSTP, DM_REC2, DM_PLY2, _______, _______, _______, KC_DEL,
+  _______, QK_BOOT, _______, DM_REC1, DM_PLY1, DM_RSTP, DM_REC2, DM_PLY2, _______, _______, _______, KC_DEL,
   _______, _______, MI_MOD,  AU_ON,   AU_OFF,  _______, KC_HOME, QWERTY, COLEMAK, _______,  _______, _______,
   _______, AU_PREV,  AU_NEXT,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
@@ -201,6 +199,22 @@ uint8_t last_muse_note = 0;
 uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
+
+bool dip_switch_update_user(uint8_t index, bool active) {
+  switch (index) {
+    case 0:
+      if (active) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      break;
+    case 1:
+      muse_mode = active;
+      break;
+  }
+  return true;
+}
 
 void matrix_scan_user(void) {
 #ifdef AUDIO_ENABLE
